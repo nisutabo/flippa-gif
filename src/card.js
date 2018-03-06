3958ee47ae656e03157eedfd94a438e1f64a139c
feihafferkamp <fei.hafferkamp@gmail.com>
2018-03-06 16:27:27 -0500

    add grid and columns css

--- src/card.js
+++ src/card.js
@@ -1,11 +1,13 @@
 const Card= (() => {
   let displayId = 1;
+
   return class Card {
     constructor({id, name, img_url}) {
       this.id = id;
       this.name = name;
       this.img_url = img_url;
       this.displayId = displayId++;
+
       this.cardsPanel = document.getElementById('cards-panel');
       this.renderCard();
       this.flips = [];
@@ -13,8 +15,7 @@ const Card= (() => {
 
     renderCard() {
       let cardImg = document.createElement('img');
-      cardImg.setAttribute('class', 'card');
-      // cardImg.setAttribute('class', 'ui card');
+      cardImg.setAttribute('class', 'ui column');
       cardImg.setAttribute('data-id', this.id);
       cardImg.setAttribute('data-display-id', this.displayId);
       cardImg.setAttribute('src', this.img_url);
